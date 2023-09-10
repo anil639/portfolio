49c2bfffead480c3e153488cb8fc9e375f2b33e2
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-09-10 18:18:58 +0530

    link updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from './components/NavigationBar/navigationbar';
-import Home from './components/home/Home';
-import Skill from './components/Skills/Skill';
-import Projects from './components/Projects/Projects';
-import About from './components/About/About';
-import Contact from './components/ContactMe/Contact';
-import Error from './components/ErrorPage/Error';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "./components/NavigationBar/navigationbar";
+import Home from "./components/home/Home";
+import Skill from "./components/Skills/Skill";
+import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
+import Contact from "./components/ContactMe/Contact";
+import Error from "./components/ErrorPage/Error";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavigationBar />
 
       <Routes>
-        <Route path='/portfolio' element={<Home />}></Route>
-        <Route path='/skill' element={<Skill />}></Route>
-        <Route path='/project' element={<Projects />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/contactMe' element={<Contact />}></Route>
-        <Route path='*' element={<Error />}></Route >
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/skill" element={<Skill />}></Route>
+        <Route path="/project" element={<Projects />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contactMe" element={<Contact />}></Route>
+        <Route path="*" element={<Error />}></Route>
       </Routes>
-
     </div>
   );
 }
--- src/components/NavigationBar/navigationbar.js
+++ src/components/NavigationBar/navigationbar.js
@@ -1,44 +1,63 @@
-import React from 'react'
-import './navigationbar.css'
+import React from "react";
+import "./navigationbar.css";
 import { BsList } from "react-icons/bs";
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 const NavigationBar = () => {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark text-white  ">
-                <div className='container'>
-                    <NavLink className="navbar-brand" to="/portfolio"><span className='name'>&lt; Anil Kumar Nayak &#47; &gt;</span></NavLink>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className='navigation-icon'>
-                            <BsList />
-                        </span>
-                    </button>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark text-white  ">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            <span className="name">&lt; Anil Kumar Nayak &#47; &gt;</span>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navigation-icon">
+              <BsList />
+            </span>
+          </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item ">
-                                <NavLink className="nav-link" to="/portfolio">Home </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/skill">Skills</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/project">Projects</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/about">About</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/contactMe">Contact Me</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div >
-    )
-}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item ">
+                <NavLink className="nav-link" to="/">
+                  Home{" "}
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/skill">
+                  Skills
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/project">
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contactMe">
+                  Contact Me
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
